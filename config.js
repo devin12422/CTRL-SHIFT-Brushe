b22fb88a427093005fdbb1bbd1f4050eae067f62
Devin Kramer <kramerdevin321@gmail.com>
2020-07-25 07:04:33 +0000

    sleepy time

--- config.js
+++ config.js
@@ -9,18 +9,19 @@ module.exports = function(seed) {
   console.log('Seed:', seed);
   var randomFunc = seedRandom(seed);
   var random = createRandomRange(randomFunc);
+  var valuesArray = getValues();
   return {
     random: randomFunc,
     seedName: seed,
     pointilism: random(0, 0.1),
-    noiseScalar: [random(0.000001, 0.000001), random(0.0002, 0.004)],
+    noiseScalar: [0.000001,valuesArray[1]],
     globalAlpha: 0.5,
-    startArea: random(0.5, 2.0),
+    startArea: valuesArray[0],
     maxRadius: random(5, 40),
-    lineStyle: random(1) > 0.5 ? 'round' : 'square',
+    lineStyle: 'round',
     interval: random(0.001, 0.01),
-    count: Math.floor(random(500, 5000)),
-    steps: Math.floor(random(200, 800)),
+    count: valuesArray[2],//number of lines
+    steps: Math.floor(random(200, 800)),//duration
     endlessBrowser: false,
     debugLuma: false,
     backgroundScale: 1,
@@ -44,6 +45,11 @@ module.exports = function(seed) {
     return newPalette;
   }
   function getValues(){
-    
+    var newValueArray = [];
+    newValueArray.push(document.getElementById("startArea").value);
+    newValueArray.push(document.getElementById("scale").value);
+    newValueArray.push(document.getElementById("count").value);
+    console.log(newValueArray);
+    return newValueArray;
   }
 };
